f4f09f58978036f42aefc2ddf774f345bc21eaf5
feng <liyufeng1515@gmail.com>
2016-08-23 15:02:05 +0800

    main and request

--- request/urls.py
+++ request/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^hello/', views.hello,name='hello'),
+    url(r'^main/', views.main,name='main'),
 ]
--- request/views.py
+++ request/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def hello(request):
-    return HttpResponse('hello,django.')
+
+def main(request):
+    return render(request,'main.html',{})
+
+
